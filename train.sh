6a8d49815fd74e3f659f452e6e7bd02503fc0168
mcnaughtonadm <mcnaughtonadm@gmail.com>
2023-09-13 16:00:01 -0700

    Fixing train.sh

--- train.sh
+++ train.sh
@@ -4,18 +4,30 @@
 ### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
 #########################################################################
 
-
 # arg 1 CUDA_VISIBLE_DEVICES
 # arg 2 CANDLE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
-### Path to your CANDLEized model's main Python script###
-CANDLE_MODEL=/usr/local/DRPreter/train.py
-DATA_PREPROCESSOR=/usr/local/DRPreter/preprocess.sh
+### Path and Name to your CANDLEized model's main Python script###
+
+# e.g. CANDLE_MODEL=graphdrp_baseline_pytorch.py
+CANDLE_MODEL= drpreter_baseline_pytorch.py
+
+# Set env if CANDLE_MODEL is not in same directory as this script
+IMPROVE_MODEL_DIR=${IMPROVE_MODEL_DIR:-$( dirname -- "$0" )}
+
+# Combine path and name and check if executable exists
+CANDLE_MODEL=${IMPROVE_MODEL_DIR}/${CANDLE_MODEL}
+if [ ! -f ${CANDLE_MODEL} ] ; then
+	echo No such file ${CANDLE_MODEL}
+	exit 404
+fi
+
 
 if [ $# -lt 2 ] ; then
+        echo "Illegalnumber of paramaters"
         echo "Illegal number of parameters"
-        echo "CUDA_VISIBLE_DEVICES and CANDLE_DATA_DIR are required"
+        echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
         exit -1
 fi
 
@@ -29,7 +41,7 @@ elif [ $# -ge 3 ] ; then
         CUDA_VISIBLE_DEVICES=$1 ; shift
         CANDLE_DATA_DIR=$1 ; shift
 
-        # if original $3 is a file, set candle_config and passthrough $@
+        # if $3 is a file, then set candle_config
         if [ -f $CANDLE_DATA_DIR/$1 ] ; then
 		echo "$1 is a file"
                 CANDLE_CONFIG=$1 ; shift
@@ -45,32 +57,12 @@ elif [ $# -ge 3 ] ; then
         fi
 fi
 
-# Set data preprocessing command
-DPP_CMD="python ${DATA_PREPROCESSOR}"
-
 # Display runtime arguments
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
 echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
 echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
+echo "running command ${CMD}"
 
 # Set up environmental variables and execute model
-EXE_DIR=$(dirname ${CANDLE_MODEL})
-cd $EXE_DIR
-
-echo "running command ${DPP_CMD}"
-CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $DPP_CMD
-echo "running command ${CMD}"
+# source /opt/conda/bin/activate /usr/local/conda_envs/Paccmann_MCA
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
-
-# Change into directory and execute tests
-# cd ${MODEL_DIR}
-
-
-# model data processing and loading
-# python HiDRA_FeatureGeneration.py
-
-# model training
-# python HiDRA_training.py
-
-# Check if successful
-exit 0
